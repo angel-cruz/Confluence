c1fd968f1f5dc03d127145158a99fcfcc81e7248
angel cruz <byangelcruz@gmail.com>
2025-06-20 10:51:37 -0400

    adjust inline js and css

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function toggleTheme() {
-    document.body.classList.toggle('dark');
+function openSettings() {
+    alert('Theme clicked!');
 }
-
-// You could dynamically update these counts from APIs in a real dashboard
-let projectCount = 12;
-let userCount = 4;
-let messageCount = 23;
-
-document.getElementById('projectCount').textContent = projectCount;
-document.getElementById('userCount').textContent = userCount;
-document.getElementById('messageCount').textContent = messageCount;
